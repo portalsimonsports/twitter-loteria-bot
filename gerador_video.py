1c873454ca696bff58d42a26cf86be30277778bc
portalsimonsports <portalsimonsports@gmail.com>
2026-07-29 20:26:55 -0300

    Ativa correção V12 do pacote narrado

--- gerador_video.py
+++ gerador_video.py
@@ -1,8 +1,8 @@
 """Compatibilidade do gerador de vídeos do Portal SimonSports.
 
-A implementação ativa está em gerador_pacote_v11.py. O método executar mantém
+A implementação ativa está em gerador_pacote_v12.py. O método executar mantém
 compatibilidade retornando o Short, enquanto gerar_pacote produz também o vídeo
 completo horizontal com as vozes Francisca, Antônio e Thalita.
 """
 
-from gerador_pacote_v11 import *  # noqa: F401,F403
+from gerador_pacote_v12 import *  # noqa: F401,F403
